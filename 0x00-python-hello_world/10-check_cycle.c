1f98f089c765cbb6b1f6d5666775bdc06319cf5a
Kaingusaid <kaingusaid22@gmail.com>
2022-05-30 14:59:03 -0700

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,145 +1,22 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - Checks if a singly-linked list contain a cycle.
- * @list: A singly-linked list.
- *
- * Return: If there is no cycle - 0.
- *         If there is no cycle - 1.
+ * check_cycle - check if a singly linked list contains a loop
+ * @list: pointer to head of a list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle, *hare;
+	listint_t *fast = list;
+	listint_t *slow = list;
 
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
+	while (fast && (fast = fast->next))
 	{
-		if (turtle == hare)
+		if (fast == slow)
 			return (1);
 
-		turtle = turtle->next;
-		hare = hare->next->next;
+		fast = fast->next;
+		slow = slow->next;
 	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
-	return (0);
-}{
-	listint_t *turtle, *hare;
-
-	if (list == NULL || list->next == NULL)
-		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
-	{
-		if (turtle == hare)
-			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
-	}
-
 	return (0);
 }
