7b9e96ba45e0d4c6dc6a0b1bf316cdcd8972590b
Kaingusaid <kaingusaid22@gmail.com>
2022-05-30 17:13:55 -0700

    linked lists cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it.
- * @list: linked list
- * Return: a integer.
+ * check_cycle - Write a function in C that checks if
+ * a singly linked list has a cycle in it.
+ *
+ * @list: The pointer to the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *lts = list;
-	listint_t *aux = lts;
+	listint_t *currentS = list;
+	listint_t *currentF = list;
 
-	while (lts && aux && lts->next)
+	while (currentS && currentF && currentF->next)
 	{
-		aux = aux->next;
-		lts = lts->next->next;
-		if (aux == lts)
+		currentS = currentS->next;
+		currentF = currentF->next->next;
+		if (currentF == currentS)
 			return (1);
 	}
+
 	return (0);
 }
