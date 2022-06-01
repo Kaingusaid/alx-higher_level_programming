f206474505a82dffa4295b32ab9e5aa7d150c09e
Kaingusaid <kaingusaid22@gmail.com>
2022-05-31 18:52:53 -0700

    smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
+for index in range(0, 26):
+    word = ord('z') - index
+    if (index % 2 == 1):
+        word = chr(word - ord('a') + ord('A'))
+    else:
+        word = chr(word)
+    print("{}".format(word), end='')
