cb9705e35cfa24794ceb4cf6ef59d669ae836b79
Kaingusaid <kaingusaid22@gmail.com>
2022-06-21 10:30:03 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" The square class has an instance called size """
+
+
+class Square:
+    """ initializing an instance called size
+
+
+    in the square class"
+    """
+    def __init__(self, size):
+        """ size is initialized as a private attribute
+        Attribute: size
+        """
+        self.__size = size
