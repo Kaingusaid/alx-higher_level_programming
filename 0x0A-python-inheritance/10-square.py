c9c81e43ee60c1d84eb18818057b109f5438dde9
Kaingusaid <kaingusaid22@gmail.com>
2022-07-04 16:59:46 -0700

    square git commit -m square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+importing Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square inheriting class Rectangle
+    """
+
+    def __init__(self, size):
+        """
+         function for class square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        calculating area
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        finding the size
+        """
+        return f"[Rectangle] {self.__size}/{self.__size}"
