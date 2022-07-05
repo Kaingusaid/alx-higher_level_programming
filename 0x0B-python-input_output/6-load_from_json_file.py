e1e190065c76225516bbd2c0ac15ce54cf48705c
Kaingusaid <kaingusaid22@gmail.com>
2022-07-05 15:12:20 -0700

    create object from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file”"""
+
+    with open(filename, 'r') as Zura:
+        return json.load(Zura)
