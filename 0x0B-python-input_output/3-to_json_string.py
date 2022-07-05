edd6a3afddcbaffa2d57a21d4ef1bb295b066ab7
Kaingusaid <kaingusaid22@gmail.com>
2022-07-05 15:03:37 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""To JSON string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """function that retuns representation of json of an object(string)"""
+
+    return json.dumps(my_obj)
