d746f35feb068635d80c12cc9cf89c3be082821d
Kaingusaid <kaingusaid22@gmail.com>
2022-08-11 09:34:19 -0700

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Root user
+-- A script that craetes the MySQL server nd grants privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
