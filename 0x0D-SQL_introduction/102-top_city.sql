8299192f40cfe6da7c4da257575683e63c4593b1
Kaingusaid <kaingusaid22@gmail.com>
2022-08-11 09:24:17 -0700

    top city

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Temperatures #1
+-- import hbtn_0c_0 database this table dump
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
+
