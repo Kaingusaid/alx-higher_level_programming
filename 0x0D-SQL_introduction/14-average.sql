85112cfa89eb126932e7289953c43a5f6c398576
Kaingusaid <kaingusaid22@gmail.com>
2022-08-11 09:12:18 -0700

    average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Average
+-- A script that computes the score average of all records in the table second_table of the database in your MySQL
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
